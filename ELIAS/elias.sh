e2c4f1e69668ab94a581d7b37ff8b55625da00e3
Anshul <anshumitts@elk.cse.iitb.ac.in>
2023-01-30 19:33:13 +0530

    adding elias env file

--- ELIAS/elias.sh
+++ ELIAS/elias.sh
@@ -4,8 +4,12 @@ then
     exit 1
 fi
 
+echo "Creating dataset G-LF-WikiSeeAlsoTitles-300K"
 ./data.sh $1 G-LF-WikiSeeAlsoTitles-300K
+echo "Running code"
 ./run_benchmark.sh G-LF-WikiSeeAlsoTitles-300K
 
+echo "Creating dataset G-LF-AmazonTitles-1.6M"
 ./data.sh $1 G-LF-AmazonTitles-1.6M
+echo "Running code"
 ./run_benchmark.sh G-LF-AmazonTitles-1.6M
